931c9fcdbdfff83f200468a721dc30459b807f97
miKsansegundo <miguelsansegundo@gmail.com>
2016-10-22 02:13:43 +0700

    Access the container bash by a script and some dependencies: Yarn and Mocha

---
+++ scripts/bash.docker.sh
@@ -0,0 +1,21 @@
+#  .--------------------------.  #
+#    __   _____  __    __  __    #
+#   |__| |     ||  |  |  ||  |   #
+#    __  | ||  ||  |  |  ||  |   #
+#   |  | | ||  ||  |  |  ||  |   #
+#   |  | | ''  ||  |_ |  ||  |   #
+#   |  |  `-.  ||    ||  ''  |   #
+#   '--' .--'  | `---' `-----'   #
+#        '-----'                 #
+#    WHERE COOL THINGS HAPPEN    #
+#  .--------------------------.  #
+
+# Access the bash command line of the Docker container
+# environment for Node.js applications
+
+# Needs an argument to select the app (server or client)
+# by default is the server bash
+
+NAME=$1
+
+docker exec -it ${NAME:-server}.iglu /bin/bash
