4d5e90a0ed83ecb97413be45c4e11e4d81995775
miKsansegundo <miguelsansegundo@gmail.com>
2016-10-22 15:00:51 +0700

    Standard Linting

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // Constants
-const PORT = 8888;
+const PORT = 8888
 
 // App
-const app = express();
+const app = express()
 app.get('/', function (req, res) {
-    res.send('Hello world\n');
-});
+  res.send('Hello world\n')
+})
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(PORT)
+console.log('Running on http://localhost:' + PORT)
