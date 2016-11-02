985152c64c9d59e0fcdd95d26918ad8efb627dd7
miKsansegundo <miguelsansegundo@gmail.com>
2016-11-02 09:02:06 +0700

    Test setup

---
+++ micro-services/myservice/test/helpers/setup.js
@@ -0,0 +1,7 @@
+require('babel-register')
+require('babel-polyfill')
+
+/* Globals for browser
+global.document = require('jsdom').jsdom('<body><div id="app"></div></body>')
+global.window = document.defaultView
+global.navigator = window.navigator */
